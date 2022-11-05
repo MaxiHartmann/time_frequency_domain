6b64161f3e42dd3364abc4d3f3452500a26a92f4
MaxiHartmann <maxi.hartmann207@gmail.com>
2022-11-05 20:05:37 +0100

    add comments and check

--- find_base_frequencies.py
+++ find_base_frequencies.py
@@ -9,6 +9,10 @@ def gcd(a,b):
      
     # base case
     if (abs(b) < 0.001):
+        # if tolerance is increased -> higher harmonics will be detected
+        # BUT: 
+        #     higher base frequencies can be diminished, because of very high
+        #     harmonics of low base frequencies
         return a
     else:
         return (gcd(b, a - math.floor(a / b) * b))
@@ -24,7 +28,8 @@ def numbers_are_related(a, b):
     return bool_related
 
 def main():
-    freqs = [2, 2.2, 4, 4.5, 6, 2.2*4.5, 8, 9, 15, 16, 30, 32, 5333.3333, 16000, 16001]
+    freqs = [1.1, 78, 42398, 94, 22, 44, 66, 72, 11, 90, 180, 270, 270, 270, 10666.66667, 2, 2.2, 4, 4.5, 6, 2.2*4.5, 8, 9, 15, 16, 30, 32, 5333.3333, 16000, 16001]
+    freqs.sort()
 
     print("INPUT")
     print("Frequencies: {}".format(freqs))
@@ -36,20 +41,18 @@ def main():
         for col in range(dim):
             matrix[row, col] = numbers_are_related(freqs[row], freqs[col])
 
-
     upper_triangle_matrix = np.triu(matrix, 0)
-    print(upper_triangle_matrix)
+    # print(upper_triangle_matrix)
 
     # freq is basefrequency when sum of column is 1 and freq is unique
     sum_columns = upper_triangle_matrix.sum(axis=0)
-
 
     base_freqs = []
     for i, col in enumerate(sum_columns):
         if col == 1:
             base_freqs.append(freqs[i])
 
-    
+    counter = len(base_freqs)
 
     print(" Fundamental Frequency   Harmonics")
     ## Frequency and which harmonics ...
@@ -63,6 +66,11 @@ def main():
                 list_of_harmonics.append(int(freqs[j]/bf))
 
         print(f"   {bf:>14.4f}        {list_of_harmonics}")
+        counter += len(list_of_harmonics)
+
+    print("### Check ###")
+    print(f"Number of input frequencies:  {dim}")
+    print(f"Number of output frequencies: {counter}")
 
 if __name__ == "__main__":
     main()
